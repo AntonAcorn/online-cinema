3f8a9eb73d764a3340f041e021ab9d5f0811a6f6
Anton <shustikovanimation@gmail.com>
2024-12-01 17:55:58 -0500

    Add file service

---
+++ src/file/file.controller.ts
@@ -0,0 +1,29 @@
+import {
+  Controller,
+  HttpStatus,
+  ParseFilePipeBuilder,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+@Controller('file')
+export class FileController {
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({
+          fileType: 'png',
+        })
+        .build({
+          errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+        })
+    )
+    file: Express.Multer.File
+  ) {
+    return console.log(file);
+  }
+}
