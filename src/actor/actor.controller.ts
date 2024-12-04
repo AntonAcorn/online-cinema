bac91dea983e3a0a67954e92e129c7789079d92b
Anton <shustikovanimation@gmail.com>
2024-12-03 22:27:26 -0500

    Add get All actors

--- src/actor/actor.controller.ts
+++ src/actor/actor.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { ActorService } from './actor.service';
+import { Auth } from 'src/auth/decorators/auth.decorator';
 
-@Controller('actor')
-export class ActorController {}
+@Controller('actors')
+export class ActorController {
+  constructor(private readonly actorService: ActorService) {}
+
+  @Get()
+  @Auth()
+  async getAll(@Query('option') option: string) {
+    return this.actorService.getAll(option);
+  }
+
+  async getBySlug() {
+    return this.actorService.getBySlug();
+  }
+
+  async getById() {
+    return this.actorService.getById();
+  }
+
+  async createActor() {
+    return this.actorService.createActor();
+  }
+
+  async updateActor() {
+    return this.actorService.updateActor();
+  }
+
+  async deleteActor() {
+    return this.actorService.deleteActor();
+  }
+}
--- src/actor/actor.service.ts
+++ src/actor/actor.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { ModelType } from '@typegoose/typegoose/lib/types';
+import { InjectModel } from 'nestjs-typegoose';
+import { ActorModel } from './actor.model';
 
 @Injectable()
-export class ActorService {}
+export class ActorService {
+  constructor(
+    @InjectModel(ActorModel) private readonly ActorModel: ModelType<ActorModel>
+  ) {}
+  async getAll(searchTerm?: string) {
+    let options = {};
+    if (searchTerm) {
+      options = {
+        $or: [
+          {
+            name: RegExp(searchTerm, 'i'),
+          },
+          {
+            slug: RegExp(searchTerm, 'i'),
+          },
+        ],
+      };
+    }
+    return this.ActorModel.find(options);
+  }
+}
